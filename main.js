d94de18324c2e61e3430ea6b96f4c654814a645d
nduhiu17 <nduhiu254@gmail.com>
2019-04-05 21:42:50 +0300

    adjust css

--- main.js
+++ main.js
@@ -12,3 +12,13 @@
   }
 
 
+  var docWidth = document.documentElement.offsetWidth;
+
+  [].forEach.call(
+    document.querySelectorAll('*'),
+    function(el) {
+      if (el.offsetWidth > docWidth) {
+        console.log(el);
+      }
+    }
+  );
